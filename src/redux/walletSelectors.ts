62820e57cb10bd4a7d2be956604ae9d0da5da940
mhsnprvr <mhsnprvr@gmail.com>
2025-06-11 14:07:09 +0330

    initialized

--- src/redux/walletSelectors.ts
+++
@@ -1,9 +0,0 @@
-import { RootState } from './store';
-
-export const selectWalletAddress = (state: RootState) => state.wallet.address;
-export const selectWalletType = (state: RootState) => state.wallet.walletType;
-export const selectWalletProvider = (state: RootState) => state.wallet.provider;
-export const selectWalletIsConnecting = (state: RootState) => state.wallet.isConnecting;
-export const selectWalletError = (state: RootState) => state.wallet.error;
-export const selectWalletChainId = (state: RootState) => state.wallet.chainId;
-export const selectWalletBalance = (state: RootState) => state.wallet.balance; 
