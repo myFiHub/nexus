10046279fad9622391e32a6af90c1f4948246c35
mhsnprvr <mhsnprvr@gmail.com>
2025-07-09 15:32:50 +0330

    same

--- containers/ongoingOutpost/components/members/remainingTimeText.tsx
+++ containers/ongoingOutpost/components/members/remainingTimeText.tsx
@@ -2,12 +2,20 @@ import { GlobalSelectors } from "app/containers/global/selectors";
 import { useSelector } from "react-redux";
 import { onGoingOutpostSelectors } from "../../selectors";
 
-export const RemainingTimeText = ({ address }: { address: string }) => {
+export const RemainingTimeText = ({
+  address,
+  userUuId,
+}: {
+  address: string;
+  userUuId: string;
+}) => {
   const myUser = useSelector(GlobalSelectors.podiumUserInfo);
   const remainingTime = useSelector(
     onGoingOutpostSelectors.remainingTime(address)
   );
   const outpost = useSelector(onGoingOutpostSelectors.outpost);
-  const isCreator = myUser?.uuid === outpost?.creator_user_uuid;
+
+  const isCreator = userUuId === outpost?.creator_user_uuid;
+
   return <>{!isCreator ? remainingTime : "Creator"}</>;
 };
