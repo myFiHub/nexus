892917642202a91ffd20fb1d0cf3155f71e0f535
mhsnprvr <mhsnprvr@gmail.com>
2025-08-18 02:40:52 +0330

    Update index.tsx

--- components/AppLink/index.tsx
+++ components/AppLink/index.tsx
@@ -22,11 +22,16 @@ const appLinkVariants = cva(
         sm: "h-8 px-3",
         lg: "h-12 px-6",
       },
+      disabled: {
+        true: "opacity-50 pointer-events-none",
+        false: "",
+      },
     },
     defaultVariants: {
       variant: "default",
       size: "default",
       underline: true,
+      disabled: false,
     },
   }
 );
@@ -37,14 +42,29 @@ export interface AppLinkProps
   href: string;
   ignore?: boolean;
   linkProps?: Omit<LinkProps, "href">;
+  disabled?: boolean;
 }
 
 const AppLink = React.forwardRef<HTMLAnchorElement, AppLinkProps>(
-  ({ className, variant, size, href, linkProps, underline, ...props }, ref) => {
+  (
+    {
+      className,
+      variant,
+      size,
+      href,
+      linkProps,
+      underline,
+      disabled,
+      ...props
+    },
+    ref
+  ) => {
     return (
       <Link
         href={href}
-        className={cn(appLinkVariants({ variant, size, className, underline }))}
+        className={cn(
+          appLinkVariants({ variant, size, className, underline, disabled })
+        )}
         ref={ref}
         {...linkProps}
         {...props}
