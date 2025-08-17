487e5ba47d40c8388e57b72ab6b8ce32edb5944d
mhsnprvr <mhsnprvr@gmail.com>
2025-08-18 02:39:37 +0330

    Update index.tsx

--- components/AppLink/index.tsx
+++ components/AppLink/index.tsx
@@ -37,29 +37,14 @@ export interface AppLinkProps
   href: string;
   ignore?: boolean;
   linkProps?: Omit<LinkProps, "href">;
-  disabled?: boolean;
 }
 
 const AppLink = React.forwardRef<HTMLAnchorElement, AppLinkProps>(
-  (
-    {
-      className,
-      variant,
-      size,
-      href,
-      linkProps,
-      underline,
-      disabled,
-      ...props
-    },
-    ref
-  ) => {
+  ({ className, variant, size, href, linkProps, underline, ...props }, ref) => {
     return (
       <Link
         href={href}
-        className={cn(
-          appLinkVariants({ variant, size, className, underline, disabled })
-        )}
+        className={cn(appLinkVariants({ variant, size, className, underline }))}
         ref={ref}
         {...linkProps}
         {...props}
